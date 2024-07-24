e7c5b1758ef6699dba57d863c2d697b6e312b48c
maurice522 <mauricerana@gmail.com>
2024-07-24 19:10:24 +0530

    added modal

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Script running!");
+window.digitalData = {
+    "page":{
+        "name":"Home Page",
+        "url":"https://maurice522.github.io/htmlwebsiteswa/",
+        "title": "Welcome to Swalalala life , Maurice Rana",
+        "server": "github"
+    }
+}
